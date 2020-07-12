c36ab649a20d29ae968cb6be7826e1c143a73ee9
Murilo Henrique <murilohns@gmail.com>
2020-07-12 12:22:59 -0300

    get sessions using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
-    <GlobalStyle />
+    <AuthProvider>
+      <SignIn />
+      <GlobalStyle />
+    </AuthProvider>
   </>
 );
 
