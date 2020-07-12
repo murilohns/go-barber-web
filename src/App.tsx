b646198b79fa7479fda9a40af1c24ec306a0aa33
Murilo Henrique <murilohns@gmail.com>
2020-07-12 16:13:51 -0300

    rename context to hooks and add signout function

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
-      <GlobalStyle />
     </AuthProvider>
+    <GlobalStyle />
   </>
 );
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -13,7 +13,7 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
